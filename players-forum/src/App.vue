8f8c89e05cf4ff9dd4d860c6a59825615fbca467
Joe Wilson <joe.wilson@jwils.xyz>
2024-11-18 16:23:03 -0500

    Pull `BaseLayout` component out of `App`, passing child nodes to "main" and "utility" slots

--- players-forum/src/App.vue
+++ players-forum/src/App.vue
@@ -5,9 +5,10 @@ import FavoritePlayersPane from './components/FavoritePlayersPane.vue';
 import TopPlayer from './components/TopPlayer.vue';
 import AddTopPlayerForm from './components/AddTopPlayerForm.vue';
 import { players } from '@/data/players';
+import BaseLayout from './components/BaseLayout.vue';
 
 export default defineComponent({
-  components: { FavoritePlayersPane, TopPlayer, AddTopPlayerForm },
+  components: { BaseLayout, FavoritePlayersPane, TopPlayer, AddTopPlayerForm },
   data() {
     return {
       favoriteTeam: 'Pittsburgh',
@@ -28,17 +29,25 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="pane top-players-pane">
-    <h2>Top players this week</h2>
-    <div class="top-players-grid">
-      <div v-if="players.length === 0" class="not-found">Top players were not found</div>
-      <div v-else class="player player--top" v-for="player in players" :key="player.name">
-        <TopPlayer :player="player" :favoriteTeam="favoriteTeam" @addFavorite="addFavoritePlayer" />
+  <BaseLayout>
+    <template v-slot:main>
+      <!-- v-slot:[slotname] is the longhand directive -->
+      <h2>Top players this week</h2>
+      <div class="top-players-grid">
+        <div v-if="players.length === 0" class="not-found">Top players were not found</div>
+        <div v-else class="player player--top" v-for="player in players" :key="player.name">
+          <TopPlayer
+            :player="player"
+            :favoriteTeam="favoriteTeam"
+            @addFavorite="addFavoritePlayer"
+          />
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="utility-panes">
-    <AddTopPlayerForm @addPlayer="addPlayer" />
-    <FavoritePlayersPane :favorite-players="favoritePlayers" />
-  </div>
+    </template>
+    <template #utility>
+      <!-- #[slotname] is the shorthand of v-slot:[slotname] -->
+      <AddTopPlayerForm @addPlayer="addPlayer" />
+      <FavoritePlayersPane :favorite-players="favoritePlayers" />
+    </template>
+  </BaseLayout>
 </template>
