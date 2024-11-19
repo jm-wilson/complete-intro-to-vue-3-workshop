36f7d3b99fe74e9b26f65a3f4f77f9f1f2ee580d
Joe Wilson <joe.wilson@jwils.xyz>
2024-11-19 16:52:48 -0500

    Add `UserCard` and `UsersPage` components

---
+++ cest-la-vue/src/components/UserCard.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+import type { PropType } from "vue";
+import { defineComponent } from "vue";
+import { JSONPlaceholderUser } from "../types";
+
+export default defineComponent({
+  props: {
+    as: {
+      type: String as PropType<"div" | "li">,
+      default: "div",
+    },
+    user: {
+      type: Object as PropType<JSONPlaceholderUser>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style>
+.user-card {
+  background-color: goldenrod;
+  padding: 0.5em;
+  border-radius: 10px;
+  display: grid;
+  gap: 0.5em;
+
+  .name {
+    font-weight: 700;
+  }
+}
+</style>
+
+<template>
+  <component :is="as" class="user-card">
+    <p class="name">{{ user.name }}</p>
+    <p>
+      <a :href="`mailto:${user.email}`">{{ user.email }}</a>
+    </p>
+    <p>
+      <a :href="`tel:${user.phone}`">{{ user.phone }}</a>
+    </p>
+  </component>
+</template>
