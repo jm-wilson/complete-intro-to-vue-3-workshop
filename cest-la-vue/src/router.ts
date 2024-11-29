c6691a976b04ffa9b4dd887023a48addedc46a90
Joe Wilson <joe.wilson@jwils.xyz>
2024-11-28 22:54:22 -0500

    Move all Page components into `views` directory, set up `vue-router` in `router.ts`, `main.js` and `App.vue`

---
+++ cest-la-vue/src/router.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/views/HomePage.vue"),
+    },
+    {
+      path: "/login",
+      component: () => import("@/views/LoginPage.vue"),
+    },
+    {
+      path: "/users",
+      component: () => import("@/views/UsersPage.vue"),
+    },
+  ],
+});
