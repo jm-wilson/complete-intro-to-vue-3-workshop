b80fcf15a8681b00a27c6b279018fafddf51665a
Joe Wilson <joe.wilson@jwils.xyz>
2024-11-28 22:52:04 -0500

    Move loading state back into `UsersPage`, update `useUserStore` to use separate `ref()` states instead of one `reactive()`

--- cest-la-vue/src/composables/useUserStore.ts
+++ cest-la-vue/src/composables/useUserStore.ts
@@ -1,30 +1,38 @@
-import { reactive } from "vue";
+import { ref } from "vue";
 import { JSONPlaceholderUser } from "../types";
 
-export async function useUserStore() {
-  const store = reactive({
-    users: [] as JSONPlaceholderUser[],
-    errorGettingUsers: false,
-  });
+export function useUserStore() {
+  // Seems like we have to use separate refs here (not reactive) so we can update states separately
+  // https://router.vuejs.org/guide/advanced/data-fetching.html
+  const users = ref<JSONPlaceholderUser[]>([]);
+  const loadingUsers = ref(false);
+  const errorGettingUsers = ref(false);
 
-  if (!store.errorGettingUsers) {
-    try {
-      // Artificial slowdown to see loading states
-      console.log("Waiting 500ms to show loading state...");
-      await new Promise((res) => setTimeout(res, 500));
-      console.log("Finished waiting 500ms");
+  // We can call an async IIFE here so the composable itself is not async,
+  // then we update the refs from within the async logic
+  (async function () {
+    if (!errorGettingUsers.value) {
+      loadingUsers.value = true;
+      try {
+        // Artificial slowdown to see loading states
+        console.log("Waiting 500ms to show loading state...");
+        await new Promise((res) => setTimeout(res, 500));
+        console.log("Finished waiting 500ms");
 
-      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-      if (!response?.ok) {
-        throw Error("Error fetching users");
+        if (!response?.ok) {
+          throw Error("Error fetching users");
+        }
+
+        users.value = await response.json();
+      } catch {
+        errorGettingUsers.value = true;
+      } finally {
+        loadingUsers.value = false;
       }
-
-      store.users = await response.json();
-    } catch {
-      store.errorGettingUsers = true;
     }
-  }
+  })();
 
-  return store;
+  return { users, loadingUsers, errorGettingUsers };
 }
